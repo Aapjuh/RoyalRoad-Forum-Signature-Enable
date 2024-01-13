03f3f8e2ed3d6ae3d6bdf8c8d321ce1bce67983e
Aapjuh <152358287+Aapjuh@users.noreply.github.com>
2024-01-13 06:47:10 +0100

    Update RoyalRoad-Forum-Signature-Enable.user.js

--- RoyalRoad-Forum-Signature-Enable.user.js
+++ RoyalRoad-Forum-Signature-Enable.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name			RoyalRoad Forum Signature Enable
 // @namespace		RoyalRoad Forum Signature Enable
-// @version			0.2
+// @version			0.3
 // @description		Enable signature when posting on RoyalRoad Forum.
 // @author			Aapjuh
 // @icon			https://raw.githubusercontent.com/Aapjuh/RoyalRoad-Forum-Signature-Enable/main/RR-logo.png
@@ -13,24 +13,26 @@
 // @match			https://www.royalroad.com/forums/newthread/*
 // @match			https://www.royalroad.com/forums/thread/*
 // @grant			none
-// @require			http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // ==/UserScript==
 //
-// If the page does not jump back up to the top or a comment/post change the timeout from 0 to something higher (1000=1sec,2000=2sec etc.)
-$(document).ready(function() {
-	'use strict';
-	// original scroll position
-	var scrollPos;
-	scrollPos = $(window).scrollTop();
+(function() {
+    'use strict';
 
-	// Enable signature (forced scroll down)
-	var $RRPostSigCheckbox = document.querySelector('.mt-checkbox');
-	if ($RRPostSigCheckbox) {
-		$RRPostSigCheckbox.click()
-	}
+    // If the page does not jump back up to the top or a comment/post change the timeout from 0 to something higher (1000=1sec,2000=2sec etc.)
+    $(document).ready(function() {
+        // original scroll position
+        var scrollPos;
+        scrollPos = $(window).scrollTop();
 
-	// back to original scoll position
-	setTimeout(function() {
-		$(window).scrollTop(scrollPos)
-	}, 0); // <--- Timeout timer, Change this if it doesn't scroll back to where the page should have been.
+        // Enable signature (forced scroll down)
+        var $RRPostSigCheckbox = document.querySelector('.mt-checkbox');
+        if ($RRPostSigCheckbox) {
+            $RRPostSigCheckbox.click()
+        }
+
+        // back to original scoll position
+        setTimeout(function() {
+            $(window).scrollTop(scrollPos)
+        }, 0); // <--- Timeout timer, Change this if it doesn't scroll back to where the page should have been.
+    });
 })();
